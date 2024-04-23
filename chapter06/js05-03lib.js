dcf5d647d12ca29d6eecb210542e1848d6be9ba3
Yehchan Lee <ychanlee92@gmail.com>
2024-04-23 17:50:51 +0900

    [update] 0423 수업 내용 업데이트

---
+++ chapter06/js05-03lib.js
@@ -0,0 +1,8 @@
+function mouseOver() {
+  const imgId = document.querySelector("#imgId");
+  imgId.src = "../소스/6/media/banana.png";
+}
+function mouseOut() {
+  const imgId = document.querySelector("#imgId");
+  imgId.src = "../소스/6/media/apple.png";
+}
