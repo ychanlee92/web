2e32b6e4ff144029fbbaae94a94cfcd1fe41759d
Yehchan Lee <ychanlee92@gmail.com>
2024-04-26 17:51:37 +0900

    0426 업데이트

---
+++ 웹사이트메인화면과제/website.main.js
@@ -0,0 +1,36 @@
+function loginWindow() {
+  let popupWidth = 600;
+  let popupHeight = 600;
+  let popupX = window.screen.width / 2 - popupWidth / 2;
+  let popupY = window.screen.height / 2 - popupHeight / 1.5;
+  window.open(
+    "",
+    "",
+    "status=no, height=" +
+      popupHeight +
+      ", width=" +
+      popupWidth +
+      ", left=" +
+      popupX +
+      ", top=" +
+      popupY
+  );
+}
+function signUpWindow(){
+  let popupWidth = 600;
+  let popupHeight = 600;
+  let popupX = window.screen.width / 2 - popupWidth / 2;
+  let popupY = window.screen.height / 2 - popupHeight / 1.5;
+  window.open(
+    "",
+    "",
+    "status=no, height=" +
+      popupHeight +
+      ", width=" +
+      popupWidth +
+      ", left=" +
+      popupX +
+      ", top=" +
+      popupY
+  );
+}
